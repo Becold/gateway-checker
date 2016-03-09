18d4e6686c452cfe37b606bb5b855cb89315f867
Rominou <befire@hotmail.fr>
2016-03-09 18:01:47 +0100

    Logs are now stocked in files

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // Dépendances
-Log = require('./lib/Log');
+GLOBAL.Log = require('./lib/Log');
 var PingtestEvent = require('./lib/PingtestEvents');
 var SpeedtestEvent = require('./lib/SpeedtestEvents');
 
 // Config
 var config = {
-    log: {
+    logs: {
         logPath: "logs/logs.log"
     },
     ping: {
-        delay: 15*1000, // 15 secondes
+        delay: 150*1000, // 15 secondes
         hosts: ['192.168.0.1', 'google.com', 'yahoo.fr']
     },
     speedtest: {
-        delay: 5*60*1000, // 5 minutes
-        logsFolderPath: "/logs/speedtest/"
+        delay: 5*1000, // 5 minutes
+        logsFolderPath: "logs/speedtest/"
     }
 };
 
