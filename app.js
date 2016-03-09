0cb34ab54797fd4f722a3b20bf7fc8cab7b7119e
Rominou <befire@hotmail.fr>
2016-03-09 17:15:23 +0100

    Added some documentation

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ var SpeedtestEvent = require('./lib/SpeedtestEvents');
 // Config
 var config = {
     log: {
-        logPath: "logs/logs.log",
-        speedtestFolderPath: "/logs/speedtest/"
+        logPath: "logs/logs.log"
     },
     ping: {
         delay: 15*1000, // 15 secondes
         hosts: ['192.168.0.1', 'google.com', 'yahoo.fr']
     },
     speedtest: {
-        delay: 5*60*1000 // 5 minutes
+        delay: 5*60*1000, // 5 minutes
+        logsFolderPath: "/logs/speedtest/"
     }
 };
 
-
 // Initialisation des services
-Log.writeLog("[Init] Initialisation en cours");
-var Pingtest = PingtestEvent.init(config);
-var Speedtest = SpeedtestEvent.init(config);
+Log.init(config);
+PingtestEvent.init(config);
+SpeedtestEvent.init(config);
